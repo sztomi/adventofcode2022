9947d805743ece56ed614be6b3981e450f6a45b0
Tamás Szelei <szelei.t@gmail.com>
2022-12-06 12:34:16 +0100

    common: add get_chars

--- common/src/io.rs
+++ common/src/io.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::{fs::File, path::Path};
 use std::io::{BufReader, BufRead};
 use crate::result::Result;
@@ -6,4 +7,8 @@ pub fn get_lines<P: AsRef<Path>>(file_name: P) -> Result<Vec<String>> {
   let file = File::open(file_name)?;
   let reader = BufReader::new(&file);
   Ok(reader.lines().map(|line| line.unwrap()).collect())
+}
+
+pub fn get_chars<P: AsRef<Path>>(file_name: P) -> Result<Vec<char>> {
+  Ok(fs::read_to_string(file_name)?.trim().chars().collect())
 }
